86ae62e073e1f933647d1b0cd680cf45835fe6de
Jeremy Pattison <pattisonjeremy@gmail.com>
2019-05-28 13:21:01 +1000

    made shell script for running no bottle on vm

--- slurms_physical_heavy.sh
+++ slurms_physical_heavy.sh
@@ -1,85 +1,28 @@
-# aimd testing
-
-sed 's/runDDQN/runAimdJeremy/g' exp_either_heavy.slurm > output.slurm
-sed 's/sampleDDQNText/ddAimd/g' output.slurm -i
-sed "s/samplePath/ddAimd/g" output.slurm -i 
-sed 's/0 2/0 1/g' output.slurm -i
-sbatch output.slurm
-sed 's/0 1/1 1/g' output.slurm -i
-sbatch output.slurm
-sed 's/1 1/2 1/g' output.slurm -i
-sbatch output.slurm
-
-# sed 's/runDDQN/runSarsaAdditional/g' exp_either_heavy.slurm > output.slurm
-# sed 's/sampleDDQNText/ddAimdSingle/g' output.slurm -i
-# sed "s/samplePath/ddAimdSingle/g" output.slurm -i 
-# sed 's/0 2/0 1/g' output.slurm -i
-# sbatch output.slurm
-# sed 's/0 1/1 1/g' output.slurm -i
-# sbatch output.slurm
-# sed 's/1 1/2 1/g' output.slurm -i
-# sbatch output.slurm
-
-# sed 's/runDDQN/runSarsaDDQNCopy/g' exp_either_heavy.slurm > output.slurm
-# sed 's/sampleDDQNText/ddAimdRoles/g' output.slurm -i
-# sed "s/samplePath/ddAimdRoles/g" output.slurm -i 
-# sed 's/0 2/0 1/g' output.slurm -i
-# sbatch output.slurm
-# sed 's/0 1/1 1/g' output.slurm -i
-# sbatch output.slurm
-# sed 's/1 1/2 1/g' output.slurm -i
-# sbatch output.slurm
-
-# sed 's/runDDQN/runSarsaNoOverdrive/g' exp_either_heavy.slurm > output.slurm
-# sed 's/sampleDDQNText/ddAimdRolesSingle/g' output.slurm -i
-# sed "s/samplePath/ddAimdRolesSingle/g" output.slurm -i 
-# sed 's/0 2/0 1/g' output.slurm -i
-# sbatch output.slurm
-# sed 's/0 1/1 1/g' output.slurm -i
-# sbatch output.slurm
-# sed 's/1 1/2 1/g' output.slurm -i
-# sbatch output.slurm
-
-sed 's/runDDQN/runSarsaOriginal/g' exp_either_heavy.slurm > output.slurm
-sed 's/sampleDDQNText/ddAimdLoads/g' output.slurm -i
-sed "s/samplePath/ddAimdLoads/g" output.slurm -i 
-sed 's/0 2/0 1/g' output.slurm -i
-sbatch output.slurm
-sed 's/0 1/1 1/g' output.slurm -i
-sbatch output.slurm
-sed 's/1 1/2 1/g' output.slurm -i
-sbatch output.slurm
-
-# sed 's/runDDQN/runSarsaX/g' exp_either_heavy.slurm > output.slurm
-# sed 's/sampleDDQNText/ddAimdLoadsSingle/g' output.slurm -i
-# sed "s/samplePath/ddAimdLoadsSingle/g" output.slurm -i 
-# sed 's/0 2/0 1/g' output.slurm -i
-# sbatch output.slurm
-# sed 's/0 1/1 1/g' output.slurm -i
-# sbatch output.slurm
-# sed 's/1 1/2 1/g' output.slurm -i
-# sbatch output.slurm
+# No Bottle Testing for experiment one
+# this is to be run by vmMachine not spartan
 
 
-sed 's/runDDQN/runSarsaY/g' exp_either_heavy.slurm > output.slurm
-sed 's/sampleDDQNText/ddAimdBoth/g' output.slurm -i
-sed "s/samplePath/ddAimdBoth/g" output.slurm -i 
-sed 's/0 2/0 1/g' output.slurm -i
-sbatch output.slurm
-sed 's/0 1/1 1/g' output.slurm -i
-sbatch output.slurm
-sed 's/1 1/2 1/g' output.slurm -i
-sbatch output.slurm
+# #AIMD
 
 
-# sed 's/runDDQN/runSarsaZ/g' exp_either_heavy.slurm > output.slurm
-# sed 's/sampleDDQNText/ddAimdBothSingle/g' output.slurm -i
-# sed "s/samplePath/ddAimdBothSingle/g" output.slurm -i 
-# sed 's/0 2/0 1/g' output.slurm -i
-# sbatch output.slurm
-# sed 's/0 1/1 1/g' output.slurm -i
-# sbatch output.slurm
-# sed 's/1 1/2 1/g' output.slurm -i
-# sbatch output.slurm
+python runAimdMal.py 0 1 aimdMalProper
+
+python runAimdJeremy.py 0 1 aimdJeremyProper
+
+# sarsa
+
+python runSARSA.py 0 10 sarOrig
+
+python runSarsaAdditional.py 0 10 sarOrigLengthened
+
+python runSarsaOriginal.py 0 10 sarHierOriginalReward
+
+
+python runDDQNHundred.py 0 10 ddHierOrigReward
+
+
+
+
+
 
 
