3d83eecf0b0f8f57c8ad5d90b14c444bcea7a0a3
Jeremy Pattison <pattisonjeremy@gmail.com>
2019-05-30 14:54:10 +1000

    allowd adversary to continue

--- adversary/ddAdvGenericAgent.py
+++ adversary/ddAdvGenericAgent.py
@@ -105,6 +105,9 @@ class ddGenAgent():
     def loadModel(self, load_path):
         print("Loading Model...")
         print(load_path)
+        
+        if not os.path.exists(load_path):
+            return -1
         ckpt = tf.train.get_checkpoint_state(load_path)
         self.saver.restore(self.sess,ckpt.model_checkpoint_path)
         checkpoint_log = open(load_path+"/checkpoint")
